2a1f5d7cb2521f091d8d1bf4b1ae8d831a2c1df7
Rodrigo Almanza <152334277+AlmanzaRodrigo@users.noreply.github.com>
2025-06-14 15:02:34 -0300

    final commit

--- app/layers/transport/transport.py
+++ app/layers/transport/transport.py
@@ -2,27 +2,50 @@
 
 import requests
 from ...config import config
+import os
+import pickle
+
+CURRENT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+
+def save_pkl_file(data):
+    file = os.path.join(CURRENT_DIRECTORY, "response.pkl")
+    
+    with open(file, "wb") as f:
+        pickle.dump(data, f)
+
+def load_pkl_file():
+    file = os.path.join(CURRENT_DIRECTORY, "response.pkl")
+
+    with open(file, "rb") as f:
+        data = pickle.load(f)
+
+    return data
 
 # comunicación con la REST API.
 # este método se encarga de "pegarle" a la API y traer una lista de objetos JSON.
 def getAllImages():
-    json_collection = []
-    for id in range(1, 30):
-        response = requests.get(config.STUDENTS_REST_API_URL + str(id))
+    if not os.path.exists(os.path.join(CURRENT_DIRECTORY, "response.pkl")):
+        json_collection = []
+        for id in range(1, 252):
+            response = requests.get(config.STUDENTS_REST_API_URL + str(id))
 
-        # si la búsqueda no arroja resultados, entonces retornamos una lista vacía de elementos.    
-        if not response.ok:
-            print(f"[transport.py]: error al obtener datos para el id {id}")
-            continue
+            # si la búsqueda no arroja resultados, entonces retornamos una lista vacía de elementos.    
+            if not response.ok:
+                print(f"[transport.py]: error al obtener datos para el id {id}")
+                continue
 
-        raw_data = response.json()
+            raw_data = response.json()
 
-        if 'detail' in raw_data and raw_data['detail'] == 'Not found.':
-            print(f"[transport.py]: Pokémon con id {id} no encontrado.")
-            continue
+            if 'detail' in raw_data and raw_data['detail'] == 'Not found.':
+                print(f"[transport.py]: Pokémon con id {id} no encontrado.")
+                continue
 
-        json_collection.append(raw_data)
-
+            json_collection.append(raw_data)
+        
+        save_pkl_file(json_collection)
+    else:
+        json_collection = load_pkl_file()
+            
     return json_collection
 
 # obtiene la imagen correspodiente para un type_id especifico 
--- app/views.py
+++ app/views.py
@@ -6,17 +6,37 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 
 def index_page(request):
+    """
+    Una funcion simple que envia a renderizar la pagina
+    de inicio.
+    """
     return render(request, 'index.html')
 
 # esta función obtiene 2 listados: uno de las imágenes de la API y otro de favoritos, ambos en formato Card, y los dibuja en el template 'home.html'.
 def home(request):
-    images = []
+    """
+    Una funcion simple que se encarga de enviar a renderizar
+    las imagenes en forma de tarjetas. Tambien envia informacion
+    sobre los favoritos del usuario para renderizar el botón
+    correspondiente.
+    """
+    images = services.getAllImages()                            # Gody//
     favourite_list = []
 
+    for img_card in services.getAllFavourites(request):         # Gody//
+        favourite_list.append(img_card.name)                    # Gody//
+    
     return render(request, 'home.html', { 'images': images, 'favourite_list': favourite_list })
 
 # función utilizada en el buscador.
 def search(request):
+    """
+    Una funcion simple que recibe como argumento una peticion http con
+    informacion del nombre que el usuario esta buscando. Realiza la
+    busqueda y envia a renderizar las tarjetas resultantes de la busqueda.
+    Tambien envia informacion de los favoritos para que se renderice
+    correctamente el boton favoritos.
+    """
     name = request.POST.get('query', '')
 
     # si el usuario ingresó algo en el buscador, se deben filtrar las imágenes por dicho ingreso.
@@ -24,18 +44,40 @@ def search(request):
         images = []
         favourite_list = []
 
+        for img_card in services.getAllFavourites(request):     # Gody//
+            favourite_list.append(img_card.name)                # Gody//lista los nombres de pokemon en favoritos
+
+        for img_card in services.getAllImages():                # Gody//filtra las imagenes por nombre
+            if img_card.name == name.lower():                   # Gody//
+                images.append(img_card)                         # Gody//
+        
         return render(request, 'home.html', { 'images': images, 'favourite_list': favourite_list })
     else:
         return redirect('home')
 
 # función utilizada para filtrar por el tipo del Pokemon
 def filter_by_type(request):
+    """
+    Una funcion simple que recibe como argumento una peticion http con
+    informacion del tipo que el usuario esta buscando. Realiza la
+    busqueda y envia a renderizar las tarjetas resultantes de la busqueda.
+    Tambien envia informacion de los favoritos para que se renderice
+    correctamente el boton favoritos.
+    """
     type = request.POST.get('type', '')
+    print(type)
 
     if type != '':
         images = [] # debe traer un listado filtrado de imágenes, segun si es o contiene ese tipo.
         favourite_list = []
 
+        for img_card in services.getAllFavourites(request):     # Gody//
+            favourite_list.append(img_card.name)                # Gody//lista los nombres de pokemon en favoritos
+
+        for img_card in services.getAllImages():                # Gody//un bucle que filtra las imagenes por tipo
+            if type in img_card.types:                          # Gody//
+                images.append(img_card)                         # Gody//
+        
         return render(request, 'home.html', { 'images': images, 'favourite_list': favourite_list })
     else:
         return redirect('home')
@@ -43,17 +85,43 @@ def filter_by_type(request):
 # Estas funciones se usan cuando el usuario está logueado en la aplicación.
 @login_required
 def getAllFavouritesByUser(request):
-    pass
+    """
+    Una funcion simple que recibe como argumento una peticion http
+    con la informacion del usuario, si es que inició sesion. Y envia a
+    renderizar, usando la plantilla favoritos, las imagenes de los 
+    pokemon que esten agregados a favoritos.
+    """
+    favourite_list = services.getAllFavourites(request)         # Gody//
+    return render(request, 'favourites.html', { 'favourite_list': favourite_list }) # Gody//
 
 @login_required
 def saveFavourite(request):
-    pass
+    """
+    Una funcion simple que recibe como argumento una peticion http
+    con la informacion de la tarjeta que el usuario que agregar a 
+    favoritos. Se agrega la tajeta a favoritos y se vuelve a cargar la
+    pantalla de galeria.
+    """
+    services.saveFavourite(request)
+    return home(request)
 
 @login_required
 def deleteFavourite(request):
-    pass
+    """
+    Una funcion simple que recibe como argumento una peticion http
+    con la informacion de la tarjeta que el usuario quiere quitar de 
+    favoritos. Se elimina la tajeta de favoritos y se vuelve a cargar la
+    pantalla favoritos.
+    """
+    services.deleteFavourite(request)
+    return getAllFavouritesByUser(request)
 
 @login_required
 def exit(request):
+    """
+    Una funcion simple que permite cerrar la sesion del usuario.
+    Luego de cerrar sesion, se redirije al usuario a la pantalla
+    de inicio.
+    """
     logout(request)
     return redirect('home')
